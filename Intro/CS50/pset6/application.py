0e2a1bbdb9733e46ecec179f37301875b7ce87bb
Connor Ross <coross3@outlook.com>
2018-04-23 18:22:21 +0800

    Create application.py

---
+++ Intro/CS50/pset6/application.py
@@ -0,0 +1,50 @@
+from flask import Flask, redirect, render_template, request, url_for
+
+import helpers
+import os
+import sys
+from analyzer import Analyzer
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+
+    # validate screen_name
+    screen_name = request.args.get("screen_name", "")
+    if not screen_name:
+        return redirect(url_for("index"))
+
+    # get screen_name's tweets
+    tweets = helpers.get_user_timeline(screen_name)
+
+    # absolute paths to lists
+    positives = os.path.join(sys.path[0], "positive-words.txt")
+    negatives = os.path.join(sys.path[0], "negative-words.txt")
+
+    # instantiate analyzer
+    analyzer = Analyzer(positives, negatives)
+
+    # calculate scores
+    positive, negative, neutral = 0, 0, 0
+    for tweet in tweets:
+        score = analyzer.analyze(tweet)
+        if score < 0:
+            negative += 1
+        if score > 0:
+            positive += 1
+        if score == 0:
+            neutral += 1
+    positive /= len(tweets)
+    negative /= len(tweets)
+    neutral /= len(tweets)
+
+    # generate chart
+    chart = helpers.chart(positive, negative, neutral)
+
+    # render results
+    return render_template("search.html", chart=chart, screen_name=screen_name)
